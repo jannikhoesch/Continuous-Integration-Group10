c195c56651a3119b442e4454669bd446e41925a2
Amanda Ekholm <amanda.margareta.ekholm@gmail.com>
2025-02-08 21:00:05 +0100

    feat/add repository cloning
    fixes #13

--- ContinuousIntegrationServer.py
+++ ContinuousIntegrationServer.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Request
 import subprocess
 import uvicorn
+import tempfile
 
 app = FastAPI()
 
@@ -12,6 +13,10 @@ async def handle(request: Request):
     # Extract repository URL and branch info
     repo_url = data.get("repository", {}).get("clone_url")
     branch = data.get("ref").split("/")[-1]
+    commit_sha = data.get("after")
+
+    with tempfile.TemporaryDirectory() as temp_dir:
+        clone_repo(repo_url, branch, commit_sha, temp_dir)
 
     # here you do all the continuous integration tasks
     # for example
@@ -20,6 +25,11 @@ async def handle(request: Request):
 
     return {"message": "CI job done"}
 
+def clone_repo(repo_url, branch, commit_sha, dir):
+    clone_cmd = f"git clone {repo_url} {dir}"
+    checkout_cmd = f"cd {dir} && git checkout {branch} && git reset --hard {commit_sha}"
+    subprocess.run(clone_cmd, shell=True, check=True, capture_output=True, text=True)
+    subprocess.run(checkout_cmd, shell=True, check=True, capture_output=True, text=True)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8010)
