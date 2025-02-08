ceb7c3e81db70a44d4b5806bc80a9459ed4c2e8f
Amanda Ekholm <amanda.margareta.ekholm@gmail.com>
2025-02-08 21:21:25 +0100

    feat/add automated test execution
    fixes #15

--- ContinuousIntegrationServer.py
+++ ContinuousIntegrationServer.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Request
 import subprocess
 import uvicorn
 import tempfile
+import os
 
 app = FastAPI()
 
@@ -17,11 +18,14 @@ async def handle(request: Request):
 
     with tempfile.TemporaryDirectory() as temp_dir:
         clone_repo(repo_url, branch, commit_sha, temp_dir)
+        environment = get_environment(temp_dir)
+        
+        #add compilation here
 
-    # here you do all the continuous integration tasks
-    # for example
-    # 1st clone your repository
-    # 2nd compile the code
+        test_success, test_output = run_tests(environment, temp_dir, commit_sha)
+        test_status = "success" if test_success else "failure"
+        print(test_status)
+        print(test_output)
 
     return {"message": "CI job done"}
 
@@ -30,6 +34,23 @@ def clone_repo(repo_url, branch, commit_sha, dir):
     checkout_cmd = f"cd {dir} && git checkout {branch} && git reset --hard {commit_sha}"
     subprocess.run(clone_cmd, shell=True, check=True, capture_output=True, text=True)
     subprocess.run(checkout_cmd, shell=True, check=True, capture_output=True, text=True)
+
+def get_environment(temp_dir):
+    # Detect build system and compile
+    if os.path.exists(f"{temp_dir}/Makefile"):
+        return "Make"
+    elif os.path.exists(f"{temp_dir}/setup.py"):
+        return "Python"
+    elif os.path.exists(f"{temp_dir}/pom.xml"):
+        return "Maven"
+    else:
+        return False, "No recognized build system found (Makefile, setup.py, pom.xml)."
+
+def run_tests(environment, temp_dir, commit_sha):
+    if environment == "Python":
+        test_cmd = f"cd {temp_dir} && python -m unittest discover tests"
+    result = subprocess.run(test_cmd, shell=True, capture_output=True, text=True)
+    return (result.returncode == 0, f"Commit {commit_sha}\n" + result.stdout + result.stderr)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8010)
