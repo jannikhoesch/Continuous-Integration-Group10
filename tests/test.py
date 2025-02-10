002ab0d533ca5e463681a215dc89e54102a36908
Amanda Ekholm <amanda.margareta.ekholm@gmail.com>
2025-02-10 18:04:56 +0100

    feat: add package for tests

---
+++ tests/test.py
@@ -0,0 +1,11 @@
+import unittest
+
+class TestFunctions(unittest.TestCase):
+
+    # structure of how the tests should be written.
+    # write 'test_' followed by a name that describes what the test does.
+    def test_nameOfTest(self):
+        self.assertTrue(True)
+
+if __name__ == '__main__':
+    unittest.main()
