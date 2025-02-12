ae4fc320b6c94ec67f5261bd90535a1270e0003d
Jannik Hösch <133743123+jannikhoesch@users.noreply.github.com>
2025-02-12 14:04:59 +0100

    fix: Add root dir to Python path (#44)
    Fixes #40

--- src/ContinuousIntegrationServer.py
+++ src/ContinuousIntegrationServer.py
@@ -6,9 +6,12 @@ import uvicorn
 import logging
 import uuid
 from datetime import datetime
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from src.Database import Database
 from src.CommitStatus import send_commit_status
 import tempfile
+
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
